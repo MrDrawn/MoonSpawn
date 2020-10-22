30293c96b23bbd6bd615d3bb2466d702ce8bd788
MrDrawn <lipelimasilva123@gmail.com>
2020-10-21 22:03:34 -0300

    First.

--- src/com/moon/spawn/commands/MoonSpawnCommand.java
+++ src/com/moon/spawn/commands/MoonSpawnCommand.java
@@ -1,6 +1,8 @@
 package com.moon.spawn.commands;
 
 import com.moon.Moon;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,6 +11,12 @@ public class MoonSpawnCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!sender.hasPermission("moon.commands.spawn.reload")) {
+            for(String message : Moon.getInstance().getConfig().getStringList("mensagens.permission")) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+            }
+            return true;
+        }
         if (args.length == 0) {
             Moon.getInstance().reloadConfig();
             sender.sendMessage(" ");
