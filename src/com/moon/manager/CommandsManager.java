3ae12b88f694051e4d7c019e8322017eca0612d1
MrDrawn <lipelimasilva123@gmail.com>
2020-10-21 22:19:04 -0300

    Comando /moonspawn adicionado.

--- src/com/moon/manager/CommandsManager.java
+++ src/com/moon/manager/CommandsManager.java
@@ -1,6 +1,7 @@
 package com.moon.manager;
 
 import com.moon.Moon;
+import com.moon.spawn.commands.MoonSpawnCommand;
 import com.moon.spawn.commands.SetSpawnCommand;
 import com.moon.spawn.commands.SpawnCommand;
 
@@ -8,10 +9,12 @@ public class CommandsManager {
 
     private SetSpawnCommand setSpawnCommand = new SetSpawnCommand();
     private SpawnCommand spawnCommand = new SpawnCommand();
+    private MoonSpawnCommand moonSpawnCommand = new MoonSpawnCommand();
 
     public CommandsManager (Moon moon) {
         moon.getInstance().getCommand("setspawn").setExecutor(setSpawnCommand);
         moon.getInstance().getCommand("spawn").setExecutor(spawnCommand);
+        moon.getInstance().getCommand("moonspawn").setExecutor(moonSpawnCommand);
     }
 
 }
